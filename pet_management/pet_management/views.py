0a6052407ded479526db0e132d5f6ec5831e2fbf
xrui112 <126767187+xrui112@users.noreply.github.com>
2024-07-09 20:32:21 +0800

    Delete pet_management/pet_management directory

--- pet_management/pet_management/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-def hello(request):
-    context = {}
-    context['hello']="welcome it"
-    return render(request,'first.html',context)
