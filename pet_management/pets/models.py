c3489ce02f171f60ff8887969cd138df15b9c608
xrui112 <126767187+xrui112@users.noreply.github.com>
2024-07-09 20:32:35 +0800

    Delete pet_management/pets directory

--- pet_management/pets/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Pet(models.Model):
-    name = models.CharField(max_length=80)
-    score = models.IntegerField(default=100)
-
-
-    def __str__(self):
-        return self.name
